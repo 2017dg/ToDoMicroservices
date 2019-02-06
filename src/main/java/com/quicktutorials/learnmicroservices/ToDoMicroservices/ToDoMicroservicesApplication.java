909c82ce6e0ca12a7514bc7dc71c64c486e117b4
durga <2017durga@gmail.com>
2019-02-05 21:59:49 -0600

    added boilerplate code

---
+++ src/main/java/com/quicktutorials/learnmicroservices/ToDoMicroservices/ToDoMicroservicesApplication.java
@@ -0,0 +1,14 @@
+package com.quicktutorials.learnmicroservices.ToDoMicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoMicroservicesApplication.class, args);
+	}
+
+}
+
